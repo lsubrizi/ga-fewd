bffa49a461802c994d46094469720bd82a9bd5b6
lsubrizi <lisas@BPSR2.local>
2016-11-23 16:24:53 -0500

    d11 lab

--- d11/materials/temperature-converter/js/project.js
+++ d11/materials/temperature-converter/js/project.js
@@ -1,4 +1,5 @@
 $("button").on("click", function() {
-  var temp = $("input").val();
-  $("#converted").text(temp);
+  var celsius = $("input").val();
+  var fahrenheit = Math.round(celsius * 9 / 5 + 32);
+  $("#converted").text(fahrenheit);
 });
