b6564dae98f33a8ec09271cdb721e7e6f4c300a0
lsubrizi <lisas@BPSR2.local>
2016-12-08 12:06:31 -0500

    d15 work

--- d14/materials/carousel/js/project.js
+++ d14/materials/carousel/js/project.js
@@ -1,15 +1,25 @@
-
-// when the right button is clicked, show the next image as selected
+$("#left").on("click", function() {
+  var current = $("img.selected")
+  var number = parseInt(current.attr("id"));
+  current.removeClass("selected");
+  if (number === 1) {
+    var prev = 4;
+  } else {
+    var prev = number - 1;
+  }
+  $("#" + prev).addClass("selected");
+  $("span#number").text(prev);
+});
 
 $("#right").on("click", function() {
-	console.log("right button clicked!")
-	var img = $()
-	$(".selected").attr("src", img.attr("src"));
+  var current = $("img.selected")
+  var number = parseInt(current.attr("id"));
+  current.removeClass("selected");
+  if (number === 4) {
+    var next = 1;
+  } else {
+    var next = number + 1;
+  }
+  $("#" + next).addClass("selected");
+  $("span#number").text(next);
 });
-
-// when the left button is clicked, show the previous image as selected
-
-$("#left").on("click", function() {
-	console.log("left button clicked!")
-});
-
---
+++ d15/owl-carousel/js/project.js
@@ -0,0 +1,7 @@
+$("#owl-example").owlCarousel({
+	navigation : false, // Show next and prev buttons, make false to hide
+	slideSpeed : 300, // miliseconds
+	paginationSpeed : 400,
+	singleItem: true, // false shows all images
+	autoPlay: 5000, //to rotate automatically
+});
